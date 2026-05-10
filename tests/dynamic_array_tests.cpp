680eb9c756eda2cd8be4217992d1044efb36c8ab
cleverkirik <k7lisenk@gmail.com>
2026-05-10 14:26:15 +0300

    darr tests

--- tests/dynamic_array_tests.cpp
+++ tests/dynamic_array_tests.cpp
@@ -13,9 +13,8 @@ TEST(dynamic_array, size_ctor) {
 
     EXPECT_EQ(arr.size(), 5);
 
-    EXPECT_EQ(arr.get(0), 0);
-    EXPECT_EQ(arr.get(1), 0);
-    EXPECT_EQ(arr.get(2), 0);
+    for (auto i = 0; i < 3; i++)
+        EXPECT_EQ(arr[i], 0);
 }
 
 TEST(dynamic_array, ptr_ctor) {
@@ -25,9 +24,8 @@ TEST(dynamic_array, ptr_ctor) {
 
     EXPECT_EQ(arr.size(), 3);
 
-    EXPECT_EQ(arr.get(0), 1);
-    EXPECT_EQ(arr.get(1), 2);
-    EXPECT_EQ(arr.get(2), 3);
+    for (auto i = 0; i < 3; i++)
+        EXPECT_EQ(arr[i], data[i]);
 
     data[0] = 100;
 
@@ -36,12 +34,12 @@ TEST(dynamic_array, ptr_ctor) {
 
 TEST(dynamic_array, ilist_ctor) {
     dynamic_array<int> arr = {1, 2, 3};
+    int expected[] = {1, 2, 3};
 
     EXPECT_EQ(arr.size(), 3);
 
-    EXPECT_EQ(arr.get(0), 1);
-    EXPECT_EQ(arr.get(1), 2);
-    EXPECT_EQ(arr.get(2), 3);
+    for (auto i = 0; i < 3; i++)
+        EXPECT_EQ(arr[i], expected[i]);
 }
 
 TEST(dynamic_array, move_ctor) {
@@ -51,9 +49,8 @@ TEST(dynamic_array, move_ctor) {
 
     EXPECT_EQ(moved.size(), 3);
 
-    EXPECT_EQ(moved.get(0), 1);
-    EXPECT_EQ(moved.get(1), 2);
-    EXPECT_EQ(moved.get(2), 3);
+    for (auto i = 0; i < 3; i++)
+        EXPECT_EQ(source[i], moved[i]);
 
     EXPECT_EQ(source.size(), 0);
 }
@@ -65,11 +62,134 @@ TEST(dynamic_array, copy_ctor) {
 
     EXPECT_EQ(copy.size(), 3);
 
-    EXPECT_EQ(copy.get(0), 1);
-    EXPECT_EQ(copy.get(1), 2);
-    EXPECT_EQ(copy.get(2), 3);
+    for (auto i = 0; i < 3; i++)
+        EXPECT_EQ(copy[i], source[i]);
 
     copy[0] = 100;
 
     EXPECT_EQ(source[0], 1);
+}
+
+TEST(dynamic_array, move_operator) {
+    dynamic_array<int> source = {1, 2, 3};
+    dynamic_array<int> moved = std::move(source);
+
+    int expected[] = {1, 2, 3};
+
+    EXPECT_EQ(moved.size(), 3);
+
+    for (auto i = 0; i < 3; i++)
+        EXPECT_EQ(moved[i], source[i]);
+
+    EXPECT_EQ(source.size(), 0);
+}
+
+TEST(dynamic_array, copy_operator) {
+    dynamic_array<int> source = {1, 2, 3};
+    dynamic_array<int> copy = source;
+
+    EXPECT_EQ(copy.size(), source.size());
+
+    for (auto i = 0; i < 3; i++)
+        EXPECT_EQ(copy[i], source[i]);
+
+    copy[0] = 100;
+
+    EXPECT_EQ(source[0], 1);
+}
+
+TEST(dynamic_array, get_out_of_range) {
+    dynamic_array<int> arr = {1, 2, 3};
+
+    EXPECT_THROW(arr.get(3), std::out_of_range);
+}
+
+TEST(dynamic_array, set) {
+    dynamic_array<int> arr = {1, 2, 3};
+    arr.set(1, 100);
+
+    EXPECT_EQ(arr[1], 100);
+}
+
+TEST(dynamic_array, set_out_of_range) {
+    dynamic_array<int> arr = {1, 2, 3};
+
+    EXPECT_THROW(arr.set(10, 100), std::out_of_range);
+}
+
+TEST(dynamic_array, push_front) {
+    dynamic_array<int> arr = {2, 3};
+    arr.push_front(1);
+    int expected[] = {1, 2, 3};
+
+    EXPECT_EQ(arr.size(), 3);
+
+    for (auto i = 0; i < 3; i++)
+        EXPECT_EQ(arr[i], expected[i]);
+}
+
+TEST(dynamic_array, push_back) {
+    dynamic_array<int> arr = {1, 2};
+    arr.push_back(3);
+    int expected[] = {1, 2, 3};
+
+    EXPECT_EQ(arr.size(), 3);
+
+    for (auto i = 0; i < 3; i++)
+        EXPECT_EQ(arr[i], expected[i]);
+}
+
+TEST(dynamic_array, insert) {
+    dynamic_array<int> arr = {1, 3};
+    arr.insert(1, 2);
+    int expected[] = {1, 2, 3};
+
+    EXPECT_EQ(arr.size(), 3);
+
+    for (auto i = 0; i < 3; i++)
+        EXPECT_EQ(arr[i], expected[i]);
+}
+
+TEST(dynamic_array, insert_end) {
+    dynamic_array<int> arr = {1, 2};
+    arr.insert(2, 3);
+    int expected[] = {1, 2, 3};
+
+    EXPECT_EQ(arr.size(), 3);
+
+    for (auto i = 0; i < 3; i++)
+        EXPECT_EQ(arr[i], expected[i]);
+}
+
+TEST(dynamic_array, insert_out_of_range) {
+    dynamic_array<int> arr = {1, 2};
+
+    EXPECT_THROW(arr.insert(3, 100), std::out_of_range);
+}
+
+TEST(dynamic_array, resize_extend) {
+    dynamic_array<int> arr = {1, 2};
+    arr.resize(4);
+
+    EXPECT_EQ(arr.size(), 4);
+
+    EXPECT_EQ(arr[0], 1);
+    EXPECT_EQ(arr[1], 2);
+}
+
+TEST(dynamic_array, resize_shrink) {
+    dynamic_array<int> arr = {1, 2, 3};
+    arr.resize(2);
+
+    EXPECT_EQ(arr.size(), 2);
+
+    EXPECT_EQ(arr[0], 1);
+    EXPECT_EQ(arr[1], 2);
+}
+
+TEST(dynamic_array, resize_zero) {
+    dynamic_array<int> arr = {1, 2, 3};
+    arr.resize(0);
+
+    EXPECT_EQ(arr.size(), 0);
 }
